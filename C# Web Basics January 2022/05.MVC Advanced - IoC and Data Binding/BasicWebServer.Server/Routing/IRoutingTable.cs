86b2b0d4911b3e6eee6817ae767c3e93a859c739
Venci97 <55453086+Venci97@users.noreply.github.com>
2022-02-14 01:09:10 +0200

    Add files via upload

---
+++ C# Web Basics January 2022/05.MVC Advanced - IoC and Data Binding/BasicWebServer.Server/Routing/IRoutingTable.cs
@@ -0,0 +1,13 @@
+﻿using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Routing
+{
+    public interface IRoutingTable
+    {
+        IRoutingTable Map(Method method,
+            string path,
+            Func<Request, Response> responsFunction);
+        IRoutingTable MapGet(string path, Func<Request, Response> responsFunction);
+        IRoutingTable MapPost(string path, Func<Request, Response> responsFunction);
+    }
+}
---
+++ C# Web Basics January 2022/05.MVC Advanced - IoC and Data Binding/BasicWebServer.Server/Routing/RoutingTable.cs
@@ -0,0 +1,66 @@
+﻿using BasicWebServer.Server.Common;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+
+namespace BasicWebServer.Server.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, 
+            Dictionary<string, Func<Request, Response>>> routes;
+
+        public RoutingTable() =>
+            this.routes = new()
+            {
+                [Method.Get] = new (StringComparer.InvariantCultureIgnoreCase),
+                [Method.Post] = new (StringComparer.InvariantCultureIgnoreCase),
+                [Method.Put] = new (StringComparer.InvariantCultureIgnoreCase),
+                [Method.Delete] = new (StringComparer.InvariantCultureIgnoreCase),
+            };
+
+        public IRoutingTable Map(Method method, 
+            string path,
+            Func<Request, Response> responseFunction)
+        {
+            Guard.AgainstNull(path, nameof(path));
+            Guard.AgainstNull(responseFunction, nameof(responseFunction));
+
+            this.routes[method][path] = responseFunction;
+
+            return this;
+        }
+
+        public IRoutingTable MapGet(string path,
+            Func<Request, Response> responseFunction)
+        {
+            Guard.AgainstDuplicatedKey(routes[Method.Get], path, "RoutingTable.Get");
+            routes[Method.Get][path] = responseFunction;
+
+            return this;
+        }
+
+        public IRoutingTable MapPost(string path,
+            Func<Request, Response> responseFunction)
+        {
+            Guard.AgainstDuplicatedKey(routes[Method.Post], path, "RoutingTable.Post");
+            routes[Method.Post][path] = responseFunction;
+
+            return this;
+        }
+
+        public Response MatchRequest(Request request)
+        {
+            var requestMethod = request.Method;
+            var requestUrl = request.Url;
+
+            if (!this.routes.ContainsKey(requestMethod) || !this.routes[requestMethod].ContainsKey(requestUrl))
+            {
+                return new NotFoundResponse();
+            }
+
+            var responseFunction = this.routes[requestMethod][requestUrl];
+
+            return responseFunction(request);
+        }
+    }
+}
