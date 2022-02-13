f2918b20ef61d351d23eb027e6e24092b211dfba
Venci97 <55453086+Venci97@users.noreply.github.com>
2022-02-14 01:05:28 +0200

    Add files via upload

---
+++ C# Web Basics January 2022/05.MVC Advanced - IoC and Data Binding/BasicWebServe.Demo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using BasicWebServer.Demo.Controllers;
+using BasicWebServer.Server;
+using BasicWebServer.Server.Controllers;
+
+namespace BasicWebServer.Demo
+{
+    public class StartUp
+    {
+        public static async Task Main()
+        {
+            await new HttpServer(routes => routes
+                .MapControllers())
+                .Start();
+        }
+    }
+}
